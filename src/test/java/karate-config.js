e02ea2cc5c11d3e47aaa61e7d3b1aa4ce54e969b
hakantektas <72494835+hakantektas@users.noreply.github.com>
2024-08-22 17:05:21 +0300

    rapor ve log detayları
    Test koşum raporlarını görebilmek için config.js de ekleme yaptım

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -3,19 +3,24 @@ function fn() {
       apiUrl: 'https://testapi.macfit.com.tr:8080/api'
   };
 
+  // Loglama ayarları
+  karate.configure('logPrettyRequest', true);
+  karate.configure('logPrettyResponse', true);
+  karate.configure('report', { showLog: true, showAllSteps: true });
+
   // Giriş işlemi yaparak token'ı alalım
   var authResponse = karate.callSingle('classpath:examples/macfit/caller/loginCaller.feature', config);
 
   // Yanıtın tamamını loglayalım
   karate.log('Auth Response:', authResponse);
 
-  // Token'ı farklı yerlerde arayalım
+  // Token'ı alalım
   var token = authResponse.result && authResponse.result.accessToken ? authResponse.result.accessToken : 
               authResponse.accessToken ? authResponse.accessToken : 
               authResponse.token ? authResponse.token : 
               null;
 
-  // Token bulunduysa global olarak ayarlayalım, bulunamadıysa hata verelim
+  // Token bulunduysa global olarak ayarlayalım
   if (token) {
     config.authToken = token;
     karate.log('Logged in with token:', config.authToken);
